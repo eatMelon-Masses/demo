6115dc478060d5ab685fe27729e7ee8be582b520
eatMelon-Masses <11764404@qq.com>
2019-04-20 12:13:27 +0800

    feat: 修改api返回json

--- src/main/java/com/yiezi/demo/controller/HelloWorldController.java
+++ src/main/java/com/yiezi/demo/controller/HelloWorldController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
   @RequestMapping("/hello")
   public String helloWorld() {
-    return "helloWorld";
+    return "this is a first demo run in jenkins";
   }
 }
