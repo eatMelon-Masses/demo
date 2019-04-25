9e6feb97dfee18da2e20c03630339c1f3a3073d4
周晔 <z11764404@gmail.com>
2019-04-25 16:43:04 +0800

    feat: 测试jenkins构建

--- src/test/java/com/yiezi/demo/DemoApplicationTests.java
+++ src/test/java/com/yiezi/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.yiezi.demo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +12,7 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		aaa
+    Assert.assertEquals(1,2);
 	}
 
 }
